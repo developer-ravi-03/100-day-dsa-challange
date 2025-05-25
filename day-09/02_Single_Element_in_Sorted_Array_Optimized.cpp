7145aba5bf269f6ba940b5c9071ad40070fb5dab
Ravi Kumar <ravibseb96@gmail.com>
2025-05-25 09:40:02 +0530

    Day 9

--- day-09/02_Single_Element_in_Sorted_Array_Optimized.cpp
+++ day-09/02_Single_Element_in_Sorted_Array_Optimized.cpp
@@ -8,19 +8,44 @@
 using namespace std;
 int SingleElement(vector<int> arr)
 {
-  int size = arr.size();
-  for (int i = 0; i < size - 1; i++)
+  int n = arr.size();
+  int st = 0, end = n - 1;
+  if (n == 1) // if there are only one element in array
+    return arr[0];
+
+  while (st <= end)
   {
-    if (arr[i - 1] != arr[i] && arr[i] != arr[i + 1])
+    int mid = st + (end - st) / 2;
+
+    if (mid == 0 && arr[0] != arr[1]) // if there are only two element in array
+      return mid;
+
+    if (mid == (2 * n - 1) && arr[n - 1] != arr[n - 2]) // check last element
+      return mid;
+
+    if (arr[mid - 1] != arr[mid] && arr[mid] != arr[mid + 1])
+      return mid;
+
+    if (mid % 2 == 0) // if even
     {
-      return i;
+      if (arr[mid - 1] == arr[mid])
+        end = mid - 1;
+      else
+        st = mid + 1;
+    }
+    else // if odd
+    {
+      if (arr[mid - 1] == arr[mid])
+        st = mid + 1;
+      else
+        end = mid - 1;
     }
   }
   return -1;
 }
 int main()
 {
-  vector<int> arr = {1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6};
+  vector<int> arr = {0, 0, 1};
   int index = SingleElement(arr);
   cout << "Index of single element : " << index << endl;
   return 0;
