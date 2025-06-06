70b880a5a32a39a29e4a83869c181c9d577520a4
Ravi Kumar <ravibseb96@gmail.com>
2025-06-06 12:43:14 +0530

    day 24 --> subarray sum equals k bruteforce and optimisez approach done

---
+++ day-24/01_SubarraySum_equals_K.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int subarraySum(vector<int> &nums, int k)
+{
+  int n = nums.size();
+  int count = 0;
+
+  for (int i = 0; i < n; i++)
+  { // starting point
+    int sum = 0;
+    for (int j = i; j < n; j++)
+    { // ending point
+      sum += nums[j];
+      if (sum == k)
+        count++;
+    }
+  }
+  return count;
+}
+int main()
+{
+  vector<int> nums = {9, 4, 20, 3, 10, 5};
+  int k = 33;
+  int ans = subarraySum(nums, k);
+  cout << "Ans : " << ans << endl;
+  return 0;
+}
---
+++ day-24/02_SubarraySum_equals_K_optimized.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int subarraySum(vector<int> &nums, int k)
+{
+  int n = nums.size();
+  int count = 0;
+  vector<int> prefixSum(n, 0);
+
+  prefixSum[0] = nums[0];
+  for (int i = 1; i < n; i++)
+  {
+    prefixSum[i] = prefixSum[i - 1] + nums[i];
+  }
+
+  unordered_map<int, int> m; // ps, freq
+  for (int j = 0; j < n; j++)
+  {
+    if (prefixSum[j] == k)
+      count++;
+
+    int val = prefixSum[j] - k;
+    if (m.find(val) != m.end())
+    {
+      count += m[val];
+    }
+
+    if (m.find(prefixSum[j]) == m.end())
+    {
+      m[prefixSum[j]] = 0;
+    }
+    m[prefixSum[j]]++;
+  }
+
+  return count;
+}
+int main()
+{
+  vector<int> nums = {9, 4, 20, 3, 10, 5};
+  int k = 33;
+  int ans = subarraySum(nums, k);
+  cout << "Ans : " << ans << endl;
+  return 0;
+}
