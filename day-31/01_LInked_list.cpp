a4b9928d785948b5fce5356ad71959def38eaeec
Ravi Kumar <ravibseb96@gmail.com>
2025-06-14 20:49:04 +0530

    Linked List -> push_back, push_front, pop_back, pop_front and print the ll completed

---
+++ day-31/01_LInked_list.cpp
@@ -0,0 +1,138 @@
+#include <iostream>
+using namespace std;
+class Node
+{
+public:
+  int data;
+  Node *next;
+
+  Node(int val)
+  {
+    data = val;
+    next = NULL;
+  }
+};
+
+class List
+{
+  Node *head;
+  Node *tail;
+
+public:
+  List()
+  {
+    head = tail = NULL;
+  }
+
+  // for push front operation
+  void push_front(int val)
+  {
+    Node *newNode = new Node(val); // dynamic allocation
+    // Node newNode(val); // static allocation (not recommended for linked lists because it will not be freed automatically)
+    if (head == NULL)
+    {
+      head = tail = newNode;
+    }
+    else
+    {
+      newNode->next = head;
+      head = newNode;
+    }
+  }
+
+  // for push front operation
+  void push_back(int val)
+  {
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+      head = tail = newNode;
+    }
+    else
+    {
+      tail->next = newNode;
+      tail = newNode;
+    }
+  }
+
+  // pop front operation
+  void pop_front()
+  {
+    if (head == NULL)
+    {
+      cout << "List is empty!" << endl;
+      return;
+    }
+    Node *temp = head;
+    head = head->next;
+    temp->next = NULL; // disconnect the node from the list
+    delete temp;       // free memory
+    if (head == NULL)  // if the list becomes empty
+      tail = NULL;
+  }
+
+  // pop back operation
+  void pop_back()
+  {
+    if (head == NULL)
+    {
+      cout << "List is empty!" << endl;
+      return;
+    }
+
+    // if (head == tail) // only one element
+    // {
+    //   delete head;
+    //   head = tail = NULL;
+    //   return;
+    // }
+    Node *temp = head;
+    while (temp->next != tail) // find the second last node
+    {
+      temp = temp->next;
+    }
+    temp->next = NULL; // disconnect the last node
+    delete tail;       // free memory of the last node
+    tail = temp;       // update tail to the second last node
+  }
+
+  // print linkedlist
+  void printll()
+  {
+    Node *temp = head;
+    while (temp != NULL)
+    {
+      cout << temp->data << " -> ";
+      temp = temp->next;
+    }
+    cout << "NULL" << endl;
+  }
+};
+
+int main()
+{
+  List ll;
+  // push front operation
+  ll.push_front(1);
+  ll.push_front(2);
+  ll.push_front(3);
+
+  // push back operation
+  ll.push_back(4);
+  ll.push_back(5);
+  ll.push_back(6);
+
+  ll.printll();
+
+  // pop front operation
+  ll.pop_front();
+  ll.pop_front();
+
+  ll.printll();
+
+  // pop back operation
+  ll.pop_back();
+  ll.pop_back();
+  ll.printll();
+  return 0;
+}
