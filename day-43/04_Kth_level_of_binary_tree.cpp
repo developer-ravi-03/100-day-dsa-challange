0ed9a6af033972879e92e74ef5eb0c7dd06e0d70
Ravi Kumar <ravibseb96@gmail.com>
2025-06-27 18:31:50 +0530

    day-43 (Kth Level of Binary Tree)

---
+++ day-43/04_Kth_level_of_binary_tree.cpp
@@ -0,0 +1,62 @@
+// TC : O(Nlogn)
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <map>
+using namespace std;
+class Node
+{
+public:
+  int data;
+  Node *left;
+  Node *right;
+
+  Node(int val)
+  {
+    data = val;
+    left = right = NULL;
+  }
+};
+
+// build tree
+static int idx = -1;
+Node *buildTree(vector<int> preorder)
+{
+  idx++;
+  if (idx >= preorder.size() || preorder[idx] == -1)
+  {
+    return NULL;
+  }
+
+  Node *root = new Node(preorder[idx]);
+  root->left = buildTree(preorder);  // left
+  root->right = buildTree(preorder); // right
+
+  return root;
+}
+
+// Kth Level of a Binary Tree
+void KthLevel(Node *root, int k)
+{
+  if (root == NULL)
+    return;
+
+  if (k == 1)
+  {
+    cout << root->data << " ";
+    return;
+  }
+
+  KthLevel(root->left, k - 1);
+  KthLevel(root->right, k - 1);
+}
+
+int main()
+{
+  vector<int> preorder = {1, 2, 7, -1, -1, -1, 3, 4, -1, -1, 5, -1, -1};
+
+  Node *root = buildTree(preorder);
+  KthLevel(root, 3);
+
+  return 0;
+}
