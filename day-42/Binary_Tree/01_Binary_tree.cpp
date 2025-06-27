97b27024dc7a4f9844e483199a5809506d6f1ea3
Ravi Kumar <ravibseb96@gmail.com>
2025-06-27 13:34:52 +0530

    day-42 (Binary Tree)

--- day-42/Binary_Tree/01_Binary_tree.cpp
+++ day-42/Binary_Tree/01_Binary_tree.cpp
@@ -33,7 +33,7 @@ Node *buildTree(vector<int> preorder)
 
 int main()
 {
-  vector<int> preorder = {1, 2, -1, -1, 3, 4, -1, 5, -1, -1};
+  vector<int> preorder = {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1};
 
   Node *root = buildTree(preorder);
   cout << root->data << endl;
