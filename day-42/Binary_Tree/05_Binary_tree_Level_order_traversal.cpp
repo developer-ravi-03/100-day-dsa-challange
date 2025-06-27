be3c1e22a4b2886f3b2ae0c76acdc965e51dc048
Ravi Kumar <ravibseb96@gmail.com>
2025-06-27 13:02:08 +0530

    day-42 (Binary Tree -> Level Order Traversal)

---
+++ day-42/Binary_Tree/05_Binary_tree_Level_order_traversal.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+class Node
+{
+public:
+  int data;
+  Node *left;
+  Node *right;
+
+  Node(int val)
+  {
+    data = val;
+    left = right = NULL;
+  }
+};
+
+// build tree
+static int idx = -1;
+Node *buildTree(vector<int> preorder)
+{
+  idx++;
+  if (idx >= preorder.size() || preorder[idx] == -1)
+  {
+    return NULL;
+  }
+
+  Node *root = new Node(preorder[idx]);
+  root->left = buildTree(preorder);  // left
+  root->right = buildTree(preorder); // right
+
+  return root;
+}
+
+// LevelOrder
+void levelOrder(Node *root)
+{
+  queue<Node *> Q;
+
+  Q.push(root);
+
+  while (Q.size() > 0)
+  {
+    Node *curr = Q.front();
+    Q.pop();
+    cout << curr->data << " ";
+
+    if (curr->left != NULL)
+    {
+      Q.push(curr->left);
+    }
+
+    if (curr->right != NULL)
+    {
+      Q.push(curr->right);
+    }
+  }
+}
+
+int main()
+{
+  vector<int> preorder = {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1};
+
+  Node *root = buildTree(preorder);
+  levelOrder(root);
+
+  return 0;
+}
