741d556785c4d10cdda09814febf0cab74e2ceee
Ravi Kumar <ravibseb96@gmail.com>
2025-07-04 07:28:59 +0530

    day-46 Min And Max Element in array some optimised

---
+++ day-46/02_Min_and_max_element_in_array_Some_optimized.cpp
@@ -0,0 +1,35 @@
+// DSA sheet
+// Min And Max Element
+// TC: O(N logn)
+// SC: O(n)
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution
+{
+public:
+  pair<int, int> getMinMax(vector<int> arr)
+  {
+    // code here
+    pair<int, int> minMax;
+    int n = arr.size();
+
+    sort(arr.begin(), arr.end());
+
+    minMax.first = arr[0];
+    minMax.second = arr[n - 1];
+
+    return minMax;
+  }
+};
+
+int main()
+{
+  vector<int> arr = {3, 1, 4, 1, 5, 9, 2, 6};
+  Solution sol;
+  pair<int, int> result = sol.getMinMax(arr);
+  cout << "Min: " << result.first << ", Max: " << result.second << endl;
+  return 0;
+}
