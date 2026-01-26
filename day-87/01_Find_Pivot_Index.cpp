4d1525759460d668fcef4ef8e4aad5c999bac0ad
Ravi Kumar <ravibseb96@gmail.com>
2026-01-26 10:49:47 +0530

    day-87

---
+++ day-87/01_Find_Pivot_Index.cpp
@@ -0,0 +1,38 @@
+//  Find Pivot Index
+// Leetcode : 724
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution
+{
+public:
+  int pivotIndex(vector<int> &nums)
+  {
+    int n = nums.size();
+    vector<int> prefix(n, 0);
+    vector<int> suffix(n, 0);
+
+    for (int i = 1; i < n; i++)
+    {
+      prefix[i] = prefix[i - 1] + nums[i - 1];
+    }
+    for (int i = n - 2; i >= 0; i--)
+    {
+      suffix[i] = suffix[i + 1] + nums[i + 1];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+      if (prefix[i] == suffix[i])
+        return i;
+    }
+    return -1;
+  }
+};
+int main()
+{
+  Solution sol;
+  vector<int> nums = {1, 7, 3, 6, 5, 6};
+  cout << sol.pivotIndex(nums) << endl;
+  return 0;
+}
